bdb120aef63bebbfcd07bc8b40ecb6c7ea5797df
Kai Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-10-12 03:49:43 -0400

    installed jade

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
 app.get('/', function(req,res) {
 	res.type('text/plain');
