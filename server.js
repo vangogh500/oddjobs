c291e0d2769c0d45fe9bbfe83e9c9b1f2c2f9e2f
Kai Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-10-12 03:44:30 -0400

    npm init and added server script

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req,res) {
+	res.type('text/plain');
+	res.status(200);
+	res.send('home');
+});
+
+app.use(function(req,res) {
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+app.use(function(req,res) {
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Express started on http://localhost:' +
+		app.get('port'));
+});
